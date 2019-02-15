582ff3172cf477df3ec21f964c61055d590829e2
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-02-15 09:53:01 +0100

    Updates

---
+++ precache-manifest.a80988b89b9ec20aedf9481d1761c963.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "419d20693ef4d91921ecb295cfd66b94",
+    "url": "/deliveroo/static/media/StratosDeliverooWeb-SemiBold.419d2069.eot"
+  },
+  {
+    "revision": "8bf8071239054e051800",
+    "url": "/deliveroo/static/css/main.c608a4b0.chunk.css"
+  },
+  {
+    "revision": "ce683961fc8261d3d7da",
+    "url": "/deliveroo/static/js/runtime~main.ce683961.js"
+  },
+  {
+    "revision": "d5fc90bdfa3cc9394668",
+    "url": "/deliveroo/static/js/2.d5fc90bd.chunk.js"
+  },
+  {
+    "revision": "5483a65377bfe153af823df819de070f",
+    "url": "/deliveroo/static/media/StratosDeliverooWeb-SemiBold.5483a653.woff2"
+  },
+  {
+    "revision": "fb0df953eeb24317c90d91aa201728c7",
+    "url": "/deliveroo/static/media/StratosDeliverooWeb-SemiBold.fb0df953.woff"
+  },
+  {
+    "revision": "10773c9896e605e94b9267fb8574d8f6",
+    "url": "/deliveroo/static/media/IBMPlexSans.10773c98.woff2"
+  },
+  {
+    "revision": "a2fc32a72e967556aa6f1d9d70051f3c",
+    "url": "/deliveroo/static/media/IBMPlexSans-Bold.a2fc32a7.woff2"
+  },
+  {
+    "revision": "beea3497b98d3d253bf4439792cfd522",
+    "url": "/deliveroo/static/media/IBMPlexSans.beea3497.woff"
+  },
+  {
+    "revision": "4acf898d1bc079e2604b2484d02add3d",
+    "url": "/deliveroo/static/media/IBMPlexSans-Bold.4acf898d.woff"
+  },
+  {
+    "revision": "8bf8071239054e051800",
+    "url": "/deliveroo/static/js/main.8bf80712.chunk.js"
+  },
+  {
+    "revision": "b10b6a2dba60c5c07a4170fe3b6b129a",
+    "url": "/deliveroo/static/media/StratosDeliverooWeb-SemiBold.b10b6a2d.ttf"
+  },
+  {
+    "revision": "cd8407ec5b173ed5ac9aa73641d8daa2",
+    "url": "/deliveroo/static/media/IBMPlexSans.cd8407ec.ttf"
+  },
+  {
+    "revision": "f0ce4da31eef04404b6ab3feef4304d1",
+    "url": "/deliveroo/static/media/IBMPlexSans-Bold.f0ce4da3.eot"
+  },
+  {
+    "revision": "f9c292e76a02eac49f225fbe4ac44e27",
+    "url": "/deliveroo/static/media/IBMPlexSans-Bold.f9c292e7.ttf"
+  },
+  {
+    "revision": "2cfc42be663be4189986053eede8b8ee",
+    "url": "/deliveroo/static/media/IBMPlexSans.2cfc42be.eot"
+  },
+  {
+    "revision": "6002eed02f1a014b233a6bba26e19c7c",
+    "url": "/deliveroo/static/media/StratosDeliverooWeb-SemiBold.6002eed0.svg"
+  },
+  {
+    "revision": "077f1ec417491e64a36064157d264f41",
+    "url": "/deliveroo/static/media/IBMPlexSans-Bold.077f1ec4.svg"
+  },
+  {
+    "revision": "17c4333d2d60bdb0def926c3e2e9f5c9",
+    "url": "/deliveroo/static/media/IBMPlexSans.17c4333d.svg"
+  },
+  {
+    "revision": "fb304a0205d3a0ca3a39ae96effce5a4",
+    "url": "/deliveroo/index.html"
+  }
+];
