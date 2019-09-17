5f7393c00d27b50a5fb065be01b76908f987d9ae
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-09-17 08:04:32 +0200

    working on testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,22 @@
 import React from "react";
-import { shallow } from "enzyme";
+// import ReactDom from "react-dom";
+import { mount, shallow } from "enzyme";
+// import { act } from "react-dom/test-utils";
 
 import App from "./App";
+
+// jest.mock("setIsError", () => ({
+//   setIsError: jest.fn()
+// }));
+// jest.mock("setLoading", () => ({
+//   setLoading: jest.fn()
+// }));
+// jest.mock("setRestaurant", () => ({
+//   setRestaurant: jest.fn()
+// }));
+// jest.mock("setMenu", () => ({
+//   setMenu: jest.fn()
+// }));
 
 jest.mock("./components/Header/Header", () => "Header");
 jest.mock("./components/Banner/Banner", () => "Banner");
@@ -11,11 +26,22 @@ jest.mock("./components/Footer/Footer", () => "Footer");
 
 describe("App", () => {
   let props;
+  const setState = jest.fn();
+  const useStateSpy = jest.spyOn(React, "useState");
+  useStateSpy.mockImplementation(init => [init, setState]);
 
   beforeEach(() => {
     props = {
       scrollToTop: jest.fn()
     };
+  });
+
+  it("should fetch data on mount", () => {
+    const wrapper = shallow(<App />);
+    // act(() => {
+    //   wrapper = ReactDom.render(<App {...props} />);
+    // });
+    // expect(wrapper.text()).toBe("");
   });
 
   it("renders the App correctly", () => {
--- src/components/Banner/Banner.test.js
+++ src/components/Banner/Banner.test.js
@@ -3,6 +3,8 @@ import { shallow } from "enzyme";
 
 import Banner from "./Banner";
 
+jest.mock("react-lines-ellipsis", () => "LinesEllipsis");
+
 describe("Banner", () => {
   let props;
 
@@ -19,39 +21,37 @@ describe("Banner", () => {
   it("renders the Banner correctly", () => {
     const wrapper = shallow(<Banner {...props} />);
     expect(wrapper).toMatchInlineSnapshot(`
-<div
-  className="banner-container"
->
-  <div
-    className="container banner"
-  >
-    <div
-      className="banner-content"
-    >
-      <h3>
-        name
-      </h3>
-      <LinesEllipsis
-        basedOn="letters"
-        className="banner-description"
-        component="div"
-        ellipsis="..."
-        maxLine="3"
-        onReflow={[Function]}
-        text="description"
-        trimRight={true}
-      />
-    </div>
-    <div
-      className="banner-img"
-      style={
-        Object {
-          "backgroundImage": "url(picture)",
-        }
-      }
-    />
-  </div>
-</div>
-`);
+      <div
+        className="banner-container"
+      >
+        <div
+          className="container banner"
+        >
+          <div
+            className="banner-content"
+          >
+            <h3>
+              name
+            </h3>
+            <LinesEllipsis
+              basedOn="letters"
+              className="banner-description"
+              ellipsis="..."
+              maxLine="3"
+              text="description"
+              trimRight={true}
+            />
+          </div>
+          <div
+            className="banner-img"
+            style={
+              Object {
+                "backgroundImage": "url(picture)",
+              }
+            }
+          />
+        </div>
+      </div>
+    `);
   });
 });
