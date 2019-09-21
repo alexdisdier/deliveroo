2be90a32e89d785fd0be7b4226f45958ba5107ed
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-09-21 14:52:47 +0200

    test

--- src/components/Section/Card/Card.js
+++ src/components/Section/Card/Card.js
@@ -5,16 +5,17 @@ import LinesEllipsis from "react-lines-ellipsis";
 
 import "./Card.css";
 
-function Card({
-  title,
-  description,
-  price,
-  picture,
-  popular,
-  id,
-  quantity,
-  addMeal
-}) {
+function Card(props) {
+  const {
+    title,
+    description,
+    price,
+    picture,
+    popular,
+    id,
+    quantity,
+    addMeal
+  } = props;
   let star = (
     <svg width="12" height="12" xmlns="http://www.w3.org/2000/svg">
       <path
