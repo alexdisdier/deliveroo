c049790498d1f20f3bfd8c67941c0bcbe0567c3e
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-09-22 06:43:39 +0200

    testing

--- src/assets/fonts/Deliveroo_files/script.js
+++ src/assets/fonts/Deliveroo_files/script.js
@@ -1,18 +1,18 @@
-const $ = document;
-const banner = $.querySelector('#js-banner');
-const headerContainer = $.querySelector('#header-container');
+// const $ = document;
+// const banner = $.querySelector('#js-banner');
+// const headerContainer = $.querySelector('#header-container');
 
-$.addEventListener("DOMContentLoaded", () => {
+// $.addEventListener("DOMContentLoaded", () => {
 
-  window.addEventListener("resize", () => {
-    if (window.innerWidth > 768) {
-      // console.log('bigger than 768px', window.innerWidth);
-      headerContainer.classList.add('container-fluid');
-      banner.classList.add('d-flex', 'align-items-center');
-    } else {
-      // console.log('smaller than 768px', window.innerWidth);
-      headerContainer.classList.remove('container-fluid');
-      banner.classList.remove('d-flex', 'align-items-center');
-    }
-  });
-});
+//   window.addEventListener("resize", () => {
+//     if (window.innerWidth > 768) {
+//       // console.log('bigger than 768px', window.innerWidth);
+//       headerContainer.classList.add('container-fluid');
+//       banner.classList.add('d-flex', 'align-items-center');
+//     } else {
+//       // console.log('smaller than 768px', window.innerWidth);
+//       headerContainer.classList.remove('container-fluid');
+//       banner.classList.remove('d-flex', 'align-items-center');
+//     }
+//   });
+// });
